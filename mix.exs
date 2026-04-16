155b59c4cf7c85cc3c514af0bbf84590d77c9882
tesguru <olaogunteslim@gmail.com>
2026-04-16 02:38:45 +0100

    push changes

--- mix.exs
+++ mix.exs
@@ -15,9 +15,6 @@ defmodule DomainOutreach.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {DomainOutreach.Application, []},
@@ -31,13 +28,9 @@ defmodule DomainOutreach.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.8.5"},
@@ -57,27 +50,20 @@ defmodule DomainOutreach.MixProject do
       {:jason, "~> 1.2"},
       {:httpoison, "~> 2.0"},
       {:dns_cluster, "~> 0.2.0"},
-          {:oban, "~> 2.19"},
+      {:oban, "~> 2.19"},
       {:bandit, "~> 1.5"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["compile", "tailwind domain_outreach", "esbuild domain_outreach"],
+      "assets.setup": ["esbuild.install --if-missing"],
+      "assets.build": ["compile", "esbuild domain_outreach"],
       "assets.deploy": [
-        "tailwind domain_outreach --minify",
         "esbuild domain_outreach --minify",
         "phx.digest"
       ],
